615a0726a1e209c5c0eae4d1870216fb7a9fec15
Howar <woohow116@gmail.com>
2021-01-03 22:43:21 +0800

    Typeface + iconfont

--- app/src/main/java/com/howar/svgdemo/MainActivity.kt
+++ app/src/main/java/com/howar/svgdemo/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.howar.svgdemo
 
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val typeface = Typeface.createFromAsset(assets, "iconfont.ttf")
+        text_icon.typeface = typeface
     }
 }
